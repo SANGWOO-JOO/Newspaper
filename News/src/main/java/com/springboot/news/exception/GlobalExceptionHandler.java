762e9b311966f597c8d088691aebd7f1e3cd4624
SANGWOO-JOO <turncoat19@gmail.com>
2022-11-16 23:35:31 +0900

    exception

---
+++ News/src/main/java/com/springboot/news/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.springboot.news.exception;
+
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    //Handle specific 
+}
